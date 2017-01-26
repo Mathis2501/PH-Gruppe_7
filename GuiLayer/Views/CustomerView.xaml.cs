139b7d8026604ef93ae1a110e7314c8a5e2f85e5
Liljeroth <chrli@DESKTOP-0SA243G>
2017-01-26 10:07:55 +0100

    CustomerView - (Work in progress) Udkommenteret forsøg på en søgefunktion.

--- GuiLayer/Views/CustomerView.xaml.cs
+++ GuiLayer/Views/CustomerView.xaml.cs
@@ -31,5 +31,30 @@ namespace GuiLayer.Views
             List<Customer> Liste = Controler.ShowCustomer();
             dataGrid.ItemsSource = Liste;
         }
+
+   /*     private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchWord = txtSearch.Text;
+
+            try
+            {
+                foreach (DataGridRow row in dataGrid.Items)
+                {
+
+                    row.IsSelected = false;
+                    if
+                        (row.
+                        Value.ToString().ToLower().Contains(searchWord.ToLower())
+                        || row.Cells[1].Value.ToString().ToLower().Contains(searchWord.ToLower())
+                        || row.Cells[2].Value.ToString().ToLower().Contains(searchWord.ToLower())
+                        || row.Cells[3].Value.ToString().ToLower().Contains(searchWord.ToLower()))
+                    {
+                        row.IsSelected = true;
+                    }
+                }
+            }
+            catch (Exception)
+            { }
+        }*/
     }
 }
